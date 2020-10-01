427082b4fb2f8c23a21f677b8391519e33088e21
scepter <zc_cold@hotmail.com>
2020-10-01 23:12:33 +0800

    ref

--- cmd/stick-local/main.go
+++ cmd/stick-local/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	server := socks5.NewServer(net.IPv4(127, 0, 0, 1), 8888)
+	server := socks5.NewServer(net.IPv4(127, 0, 0, 1), 8888, map[socks5.CMD]socks5.HandleCMDFunc{})
 	err := server.Run()
 	fmt.Println(err)
 }
